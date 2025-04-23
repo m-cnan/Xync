3383e6e4a6bb83f7282812c9e0ce1d0d5b4177c4
m-cnan <emmohammedsinan03@gmail.com>
2025-04-23 02:18:42 +0000

    media used for recieved files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'received_files')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
